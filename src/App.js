a7908b397835f8743d7f13b4747adf416c8dd35f
ConnorDBurge <connor.d.burge@gmail.com>
2022-02-07 11:19:24 -0800

    useParams added to get studentId from route

--- src/App.js
+++ src/App.js
@@ -1,18 +1,27 @@
-import React from 'react';
+import React, { useState } from 'react';
 import Navbar from './components/Navbar';
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom'
 import StudentsPage from './pages/StudentsPage';
+import StudentPage from './pages/StudentPage';
 import CoursesPage from './pages/CoursesPage'
 import HomePage from './pages/HomePage';
 
 const App = () => {
+
     return (
         <Router>
             <Navbar />
             <Routes>
                 <Route path="" element={<HomePage />} />
-                <Route path="students" element={<StudentsPage />} />
-                <Route path="courses" element={<CoursesPage />} />
+                <Route
+                    path="students"
+                    element={<StudentsPage />} />
+                <Route
+                    path="students/:studentId"
+                    element={<StudentPage />} />
+                <Route
+                    path="courses"
+                    element={<CoursesPage />} />
             </Routes>
         </Router>
     );
--- src/pages/StudentsPage.js
+++ src/pages/StudentsPage.js
@@ -18,7 +18,9 @@ const StudentPage = () => {
     const renderedStudents = students.map((student, index) => {
         return (
             <h1 key={index}>
-                <Link to="/">{student.first_name} {student.last_name}</Link>
+                <Link to={`/students/${student.id}`}>
+                    {student.first_name} {student.last_name}
+                </Link>
             </h1>
         )
     })
